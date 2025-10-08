8ef28256ed8c76ae6da58d04eed9085f22cf1db6
Breno Santiago De Sousa <dev.santiago@proton.me>
2025-10-08 11:16:57 -0300

    Add project files.

---
+++ FinanceApp/Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace FinanceApp.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = null!;
+        public decimal Amount { get; set; }
+        public string Category { get; set; } = null!;
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
